9b7e0721d8d4e7e6f74aa8c35e154e64f5248a70
Hunter <1014398744@qq.com>
2021-05-11 23:55:59 +0800

    first commit

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by Hunter on 2021-05-11.
+ */
+public class HelloWorldTest {
+
+    @Test
+    public void test(){
+        System.out.println("hello world");
+    }
+}
